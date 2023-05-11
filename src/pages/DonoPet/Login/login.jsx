1703ed4f642edf4e2918ac1c1dbad50a1f8dd872
Gabriel Nogueira da Costa <gabriel.costa@vr.com.br>
2023-05-11 20:44:34 -0300

    (Created)Criado componente de Menu -Gabriel Nogueira

--- src/pages/DonoPet/Login/login.jsx
+++ src/pages/DonoPet/Login/login.jsx
@@ -1,6 +1,6 @@
 import './styleLogin.css';
-import Background from '../../assets/images/PETSUP-BACKGROUND-COPIA.png'
-import Image from '../../assets/icons/PETSUP-LOGIN-ICON.png'
+import Background from '../../../assets/images/PETSUP-BACKGROUND-COPIA.png'
+import Image from '../../../assets/icons/PETSUP-LOGIN-ICON.png'
 
 const Login = () => {
   return (
@@ -18,15 +18,15 @@ const Login = () => {
         <div className="inputs">
           <div className="inputs-email">
           <label htmlFor="Email">E-mail</label>
-          <input onKeyUp={validarEmail} type="text" placeholder="usuario@exemplo.com"/>
+          <input type="text" placeholder="usuario@exemplo.com"/>
           <div className="frases-validacao" id="frase-email"></div>
           </div>
           <div className="inputs-password">
           <label htmlFor="Senha">Senha</label>
-          <input onKeyUp={validarSenha} type="password" placeholder="*******"/>
+          <input type="password" placeholder="*******"/>
           <div className="frases-validacao" id="frase-senha"></div>
           </div>
-          <button onClick={validarLogin} className="button-cadastro">ENTRAR</button>
+          <button className="button-cadastro">ENTRAR</button>
         </div>
       </div>
       </div>
