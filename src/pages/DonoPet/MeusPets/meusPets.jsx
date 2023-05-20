27743fc311dad37fa718557e4fcd4093c43ec061
Gabriel Nogueira da Costa <gabriel.costa@vr.com.br>
2023-05-19 21:03:13 -0300

    (Changed) Atualização da tela de meusPets

--- src/pages/DonoPet/MeusPets/meusPets.jsx
+++ src/pages/DonoPet/MeusPets/meusPets.jsx
@@ -5,8 +5,31 @@ export default function meusPets() {
 
     return (
         <div className="container-main">
-            <Menu/>
-            <div className="content">sexxxx</div>
+            <Menu />
+            <div className="content-main">
+                <h2>PETS CADASTRADOS</h2>
+
+                <div className="content">
+
+                    <div className="content2">
+
+                        <div className="teste">
+
+                            <label htmlFor="filtrar">Filtrar:</label>
+
+                            <select name="" id="filtro">
+                                <option value="value1">Nome</option>
+                                <option value="value2">Tipo</option>
+                            </select>
+                    
+                            <input type="text" />
+                            <button>ADICIONAR PET +</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
