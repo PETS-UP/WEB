657c465b5d14954bd68bebb871cf58f48ef8f659
Gabriel Nogueira da Costa <gabriel.costa@vr.com.br>
2023-05-24 19:28:16 -0300

    (Changed)Inicio da estilização da tela Home

--- src/components/Base/index.jsx
+++ src/components/Base/index.jsx
@@ -1,15 +1,17 @@
 import './styles.css';
-import Background from '../../assets/images/PETSUP-BACKGROUND-HOME.png';
-import Image from '../../assets/icons/PETSUP-LOGO-ICON.png';
+import Background from '../../assets/images/IMAGE-HOMEPAGE-PETSUP.png';
+import Logo from '../../assets/icons/ICON-LOGO.png';
+import Github from '../../assets/icons/ICON-GITHUB.png';
 
 const BaseComponent = () => {
   return (
     <div className="banner">
       <img src={Background} className="imagebg" />
-      <div className="main-content-index">
-        <div className="image-main"><img src={Image} /></div>
-        <div className="content-main">
-          <p className="title-content">Seja bem vindo! </p>
+      <div className='header'>
+        <img src={Logo} alt="" />
+        <div className='btn'>
+          <button id='btn-cliente'>ETRAR COMO CLIENTE</button>
+          <button id='btn-petshop'>ENTRAR COMO EMPRESA</button>
         </div>
       </div>
     </div>
