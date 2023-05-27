604b90967a2d30e613e3f84661bb604dd7f6f516
Gabriel Nogueira <gabriel.costa@spteh.school>
2023-05-27 20:20:22 -0300

    (Changed) alteração na tela meus Pets

--- src/pages/DonoPet/MeusPets/meusPets.jsx
+++ src/pages/DonoPet/MeusPets/meusPets.jsx
@@ -4,66 +4,95 @@ import '../MeusPets/meusPets.css'
 export default function meusPets() {
 
     return (
-        <div className="container-main">
+        <div className="container-main-meus-pets">
             <Menu />
-            <div className="content-main">
-                <h2>PETS CADASTRADOS</h2>
+            <div className="content-meus-pets">
 
-                <div className="content">
+                <div className="titulo-meus-pets">
+                    <h2>PETS CADASTRADOS</h2>
+                </div>
 
-                    <div className="content2">
+                <div className="selecionaveis-meus-pets">
+                    <div className="filtro-pets">
 
-                        <div className="teste">
+                        <label htmlFor="">Filtrar:</label>
+                        <select className="filtro-meus-pets" name="Filtrar" id="">
+                            <option value="nome">Nome</option>
+                            <option value="tipo">Tipo</option>
+                        </select>
 
-                            <label htmlFor="filtrar">Filtrar:</label>
-
-                            <select className="filtro" name="">
-                                <option value="value1">Nome</option>
-                                <option value="value2">Tipo</option>
-                            </select>
-
-                            <input type="text" />
-                            <button>ADICIONAR PET +</button>
-                        </div>
-                        <div className="tabela">
-                            <table className="table-container">
-                                <thead>
-                                    <tr>
-                                        <th>Nome</th>
-                                        <th>Tipo</th>
-                                        <th>Editar</th>
-                                        <th>Excluir</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Fluffy</td>
-                                        <td>Cachorro</td>
-                                        <td>Data 3</td>
-                                        <td>Data 4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Data 4</td>
-                                        <td>Data 5</td>
-                                        <td>Data 6</td>
-                                        <td>Data 6</td>
-                                    </tr> <tr>
-                                        <td>Data 7</td>
-                                        <td>Data 8</td>
-                                        <td>Data 9</td>
-                                        <td>Data 9</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-
-
-                        </div>
                     </div>
+                    <input type="text" />
+
+                    <button>ADICIONAR PET +</button>
+                </div>
+
+                <div className="tabela-meus-pets">
+                    <table className="table-container">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Tipo</th>
+                                <th>Editar</th>
+                                <th>Excluir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Fluffy</td>
+                                <td>Cachorro</td>
+                                <td>Data 3</td>
+                                <td>Data 4</td>
+                            </tr>
+                            <tr>
+                                <td>Data 4</td>
+                                <td>Data 5</td>
+                                <td>Data 6</td>
+                                <td>Data 6</td>
+                            </tr> <tr>
+                                <td>Data 7</td>
+                                <td>Data 8</td>
+                                <td>Data 9</td>
+                                <td>Data 9</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                 </div>
 
             </div>
 
-        </div>
+        </div >
     )
 }
+
+
+<table className="table-container">
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Tipo</th>
+            <th>Editar</th>
+            <th>Excluir</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Fluffy</td>
+            <td>Cachorro</td>
+            <td>Data 3</td>
+            <td>Data 4</td>
+        </tr>
+        <tr>
+            <td>Data 4</td>
+            <td>Data 5</td>
+            <td>Data 6</td>
+            <td>Data 6</td>
+        </tr> <tr>
+            <td>Data 7</td>
+            <td>Data 8</td>
+            <td>Data 9</td>
+            <td>Data 9</td>
+        </tr>
+    </tbody>
+</table> 
