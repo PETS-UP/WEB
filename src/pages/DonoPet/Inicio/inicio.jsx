53c6211d5332b95a1b96d7dbd559b5a431513a4a
william.matos@sptech.school <william.matos@sptech.school>
2023-05-27 03:37:48 -0300

    listagem de pets front end finalizado 2

--- src/pages/DonoPet/Inicio/inicio.jsx
+++ src/pages/DonoPet/Inicio/inicio.jsx
@@ -21,7 +21,9 @@ export default function Inicio() {
                         <button>Meus favoritos</button>
                     </div>
                 </div>
+
                 <div className="cards-petshop">
+
                     <div className="card-items-petshop">
                         <div className="card-image-petshop"><img src={ImgPetshop} /></div>
                         <div className="card-info-petshop">
@@ -33,26 +35,60 @@ export default function Inicio() {
                             <div className="card-info-petshop-content">
                                 <div className="petshop-content-title">
                                     <div className="petshop-content-servicos">
-
+                                        <p>Banho, Tosa & Outros</p>
                                     </div>
                                     <div className="petshop-content-preco">
-
+                                        <p>R$32,90</p>
                                     </div>
                                 </div>
                                 <div className="petshop-content-subtitle">
                                     <div className="petshop-content-distancia">
-
+                                    <p>1.2 KM</p>
                                     </div>
                                     <div className="petshop-content-tempo">
-                                        
+                                    <p>32 min</p>          
                                     </div>
                                     <div className="petshop-content-status">
-                                        
+                                    <p>Em funcionamento</p>  
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                 <div className="card-items-petshop">
+                        <div className="card-image-petshop"><img src={ImgPetshop} /></div>
+                        <div className="card-info-petshop">
+                            <div className="card-info-petshop-title">
+                                <div className="info-petshop-title">
+                                    <p>Fofinho PetShop</p>
+                                </div>
+                            </div>
+                            <div className="card-info-petshop-content">
+                                <div className="petshop-content-title">
+                                    <div className="petshop-content-servicos">
+                                        <p>Banho, Tosa & Outros</p>
+                                    </div>
+                                    <div className="petshop-content-preco">
+                                        <p>R$32,90</p>
+                                    </div>
+                                </div>
+                                <div className="petshop-content-subtitle">
+                                    <div className="petshop-content-distancia">
+                                    <p>1.2 KM</p>
+                                    </div>
+                                    <div className="petshop-content-tempo">
+                                    <p>32 min</p>          
+                                    </div>
+                                    <div className="petshop-content-status">
+                                    <p>Em funcionamento</p>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
