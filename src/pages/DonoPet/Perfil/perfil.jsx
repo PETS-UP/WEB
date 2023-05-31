824400bb24dec5aa5b6587bab39e69a059f80be6
MauricioMSoares <mauricio.soares@sptech.school>
2023-05-31 20:34:41 -0300

    feat: aplicando useState aos atributos da tela de perfil cliente

--- src/pages/DonoPet/Perfil/perfil.jsx
+++ src/pages/DonoPet/Perfil/perfil.jsx
@@ -1,9 +1,26 @@
+import api from '../../../api';
 import Menu from "../../../components/Base/Menu/menu";
 import './styleperfil.css';
 import '../../stylepadrao.css';
 import imgUser from '../../../assets/icons/ICON-BACHIRA-USER.jpg';
+import { useState } from 'react';
 
 export default function Inicio() {
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [dataNasc, setDataNasc] = useState("");
+    const [cep, setCep] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [imagem, setImagem] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [bairro, setBairro] = useState("");
+    const [rua, setRua] = useState("");
+
+    const [cepData, setCepData] = useState(null);
+
     return (
         <div className="container-main-perfil">
             
@@ -24,7 +41,7 @@ export default function Inicio() {
                         <div className="inputs-user-perfil">
                         <label htmlFor="Nome">Nome</label>
                             <input type="text" disabled placeholder="William Matos"/>
-                        <label htmlFor="Nome">Email</label>
+                        <label htmlFor="Nome">E-mail</label>
                             <input type="email" disabled placeholder="William.matos@sptech.school"/>
                         <label htmlFor="Nome">Data de nascimento</label>
                             <input type="text" disabled placeholder="02/08/99"/>
@@ -34,7 +51,7 @@ export default function Inicio() {
                             <input type="number" disabled placeholder="08145795-3"/>
                         <label htmlFor="Nome">CPF</label>
                             <input type="number" disabled placeholder="294.358.501-15"/>
-                        <label htmlFor="Nome">TEL</label>
+                        <label htmlFor="Nome">Telefone</label>
                             <input type="number" disabled placeholder="(11) 94956-3874"/>
                         </div>
                     </div>
