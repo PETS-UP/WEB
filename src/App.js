a513aeda420b2742dd313de007be12211dde2cde
MauricioMSoares <mauricio.soares@sptech.school>
2023-06-10 21:58:33 -0300

    feat: criação de App.js para otimizar funcionamento

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main";
+import VLibras from "@djpfs/react-vlibras";
+
+function App() {
+    return (
+        <>
+            <VLibras />
+        </>
+    )
+}
+
+export default App;
