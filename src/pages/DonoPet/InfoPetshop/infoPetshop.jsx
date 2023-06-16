87fbe78248edea4baca90b3938ee19a4e7e88ca7
belthenrique <henrique.beltrao@sptech.school>
2023-06-16 09:33:45 -0300

    tela de informação petshop

--- src/pages/DonoPet/InfoPetshop/infoPetshop.jsx
+++ src/pages/DonoPet/InfoPetshop/infoPetshop.jsx
@@ -1,17 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Calendar from "react-calendar";
+
 import api from "../../../api";
 import Menu from "../../../components/Base/Menu/menu";
-import React, { useEffect } from "react";
+
 import "../../stylepadrao.css";
 import "../InfoPetshop/styleinfopetshop.css";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
 
 export default function infoPetshop() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [idPet, setIdPet] = useState();
   const [idServico, setIdServico] = useState();
   const [listaServicos, setListaServicos] = useState([]);
+  const [listaPets, setListaPets] = useState([]);
+  const [hour, setHour] = useState();
+  const [date, setDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(null);
 
   useEffect(() => {
     api
@@ -26,6 +33,22 @@ export default function infoPetshop() {
       .then(({ data }) => {
         console.log(data);
         setListaServicos(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    api
+      .get("/pets", {
+        params: {
+          idCliente: sessionStorage.ID_CLIENTE,
+        },
+        headers: {
+          Authorization: `Bearer ${sessionStorage.JWT}`,
+        },
+      })
+      .then(({ data }) => {
+        console.log(data);
+        setListaPets(data);
       })
       .catch((error) => {
         console.log(error);
@@ -50,32 +73,61 @@ export default function infoPetshop() {
         }
       )
       .then(({ data }) => {
-        console.log("funcionou")
+        console.log("funcionou");
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  const handleDayClick = (value) => {
+    setSelectedDate(value);
+    console.log(value.toISOString());
+  };
+
   return (
     <div className="container-main">
       <Menu />
       <div className="content">
+        <div className="info-petshop-servicos">
+          <p>Selecione o Pet</p>
+          <div className="info-petshop-card-servicos">
+            {listaPets.length > 0 ? (
+              listaPets.map((pet) => (
+                <button onClick={() => setIdPet(pet.id)}>{pet.nome}</button>
+              ))
+            ) : (
+              <p>
+                Você ainda não possui pets adicione pets clicando
+                <span onClick={() => navigate(`/meus-pets`)}> aqui</span>
+              </p>
+            )}
+          </div>
+        </div>
 
         <div className="info-petshop-servicos">
           <p>Selecione o Serviço</p>
           <div className="info-petshop-card-servicos">
-            {
-                listaServicos.map((servico) => (
-                    <button>{servico.nome}</button>
-                ))
-            }
+            {listaServicos.length > 0 ? (
+              listaServicos.map((servico) => (
+                <button onClick={() => setIdServico(servico.id)}>
+                  {servico.nome}
+                </button>
+              ))
+            ) : (
+              <p>O petshop selecionado não possui serviços</p>
+            )}
           </div>
         </div>
 
         <div className="info-petshop-agenda">
           <p>Selecione a Data</p>
-          <div className="info-petshop-card-agenda"></div>
+          <div className="info-petshop-card-agenda">
+            <Calendar
+              value={date}
+              onClickDay={handleDayClick}
+            />
+          </div>
         </div>
       </div>
     </div>
