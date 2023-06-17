3eb87bf2a73b0dd960654758d8a0a9d6e4f3166b
MauricioMSoares <mauricio.soares@sptech.school>
2023-06-17 03:05:10 -0300

    fix: correção na estrutura html corrompida durante merge

--- src/pages/Petshop/PerfilPetshop/perfilPetshop.jsx
+++ src/pages/Petshop/PerfilPetshop/perfilPetshop.jsx
@@ -7,79 +7,79 @@ import { InputMask } from "primereact/inputmask";
 import api from "../../../api";
 
 const PerfilPetshop = () => {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [cep, setCep] = useState("");
-  const [cnpj, setCpf] = useState("");
-  const [telefone, setTelefone] = useState("");
-  const [imagem, setImagem] = useState("");
-  const [estado, setEstado] = useState("");
-  const [cidade, setCidade] = useState("");
-  const [bairro, setBairro] = useState("");
-  const [rua, setRua] = useState("");
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [cep, setCep] = useState("");
+    const [cnpj, setCpf] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [imagem, setImagem] = useState("");
+    const [estado, setEstado] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [bairro, setBairro] = useState("");
+    const [rua, setRua] = useState("");
 
-  useEffect(() => {
-    api
-      .get(`/petshops/${sessionStorage.ID_PETSHOP}`, {
-        params: {
-          idPetshop: sessionStorage.ID_PETSHOP,
-        },
-        headers: {
-          Authorization: `Bearer ${sessionStorage.JWT}`,
-        },
-      })
-      .then((resposta) => {
-        setNome(resposta.data.nome);
-        setEmail(resposta.data.email);
-        setCep(resposta.data.cep);
-        setCpf(resposta.data.cnpj);
-        setTelefone(resposta.data.telefone);
-      })
-      .catch((erro) => {
-        console.log(erro);
-      });
-  }, [sessionStorage.ID_PETSHOP]);
+    useEffect(() => {
+        api
+            .get(`/petshops/${sessionStorage.ID_PETSHOP}`, {
+                params: {
+                    idPetshop: sessionStorage.ID_PETSHOP,
+                },
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.JWT}`,
+                },
+            })
+            .then((resposta) => {
+                setNome(resposta.data.nome);
+                setEmail(resposta.data.email);
+                setCep(resposta.data.cep);
+                setCpf(resposta.data.cnpj);
+                setTelefone(resposta.data.telefone);
+            })
+            .catch((erro) => {
+                console.log(erro);
+            });
+    }, [sessionStorage.ID_PETSHOP]);
 
-  const buscarCep = async () => {
-    if (cep.length === 8) {
-      try {
-        const response = await api.get(`https://viacep.com.br/ws/${cep}/json/`);
-        setEstado(response.data.uf);
-        setCidade(response.data.localidade);
-        setBairro(response.data.bairro);
-        setRua(response.data.logradouro);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-
-  function atualizar(e) {
-    e.preventDefault();
-    const petshop = {
-      nome: nome,
-      email: email,
-      cep: cep,
-      cnpj: cnpj,
-      telefone: telefone,
-      estado: estado,
-      cidade: cidade,
-      bairro: bairro,
-      rua: rua,
+    const buscarCep = async () => {
+        if (cep.length === 8) {
+            try {
+                const response = await api.get(`https://viacep.com.br/ws/${cep}/json/`);
+                setEstado(response.data.uf);
+                setCidade(response.data.localidade);
+                setBairro(response.data.bairro);
+                setRua(response.data.logradouro);
+            } catch (error) {
+                console.log(error);
+            }
+        }
     };
-    api
-      .patch(`/petshops/${sessionStorage.ID_PETSHOP}`, petshop, {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.JWT}`,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((erro) => {
-        console.log(erro);
-      });
-  }
+
+    function atualizar(e) {
+        e.preventDefault();
+        const petshop = {
+            nome: nome,
+            email: email,
+            cep: cep,
+            cnpj: cnpj,
+            telefone: telefone,
+            estado: estado,
+            cidade: cidade,
+            bairro: bairro,
+            rua: rua,
+        };
+        api
+            .patch(`/petshops/${sessionStorage.ID_PETSHOP}`, petshop, {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.JWT}`,
+                },
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((erro) => {
+                console.log(erro);
+            });
+    }
 
     return (
 
@@ -109,8 +109,6 @@ const PerfilPetshop = () => {
                                 <input value={nome} onChange={(e) => setNome(e.target.value)} type="text" />
                                 <label htmlFor="Nome">E-mail</label>
                                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
-                                <label htmlFor="Nome">Data de nascimento</label>
-                                {/* <InputMask value={dataNasc} onChange={(e) => setDataNasc(e.target.value)} type="text" mask="9999-99-99" unmask="false" /> */}
                             </div>
                             <div className="inputs-user-perfil-petshop">
                                 <label htmlFor="Nome">CEP</label>
@@ -121,13 +119,14 @@ const PerfilPetshop = () => {
                                 <InputMask value={telefone} onChange={(e) => setTelefone(e.target.value)} type="text" mask="(99) 99999-9999" unmask="true" />
                             </div>
                         </div>
-          <div className="btn-atualizar-perfil-petshop">
-            <button onClick={atualizar}>Atualizar informações</button>
-          </div>
+                    </div>
+                    <div className="btn-atualizar-perfil-petshop">
+                        <button onClick={atualizar}>Atualizar informações</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PerfilPetshop;
