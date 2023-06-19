c95fb1c7021b889837b533a20acabf3f2aaf6011
MauricioMSoares <mauricio.soares@sptech.school>
2023-06-18 23:21:18 -0300

    feat: calendário de agendamentos arrumado

--- src/components/Calendario/calendario.jsx
+++ src/components/Calendario/calendario.jsx
@@ -3,48 +3,16 @@ import Calendar from "react-calendar";
 import api from "../../api"
 import "react-calendar/dist/Calendar.css";
 import "../Calendario/calendario.css";
-import { useEffect } from "react";
-
-function formatDate(date) {
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-
-    return `${day < 10 ? "0" + day : day}/${month < 10 ? "0" + month : month}/${year}`;
-}
-
-function downloadTxt() {
-    api.get(`/petshops/download/txt/${sessionStorage.ID_PETSHOP}`, {
-        headers: {
-            Authorization: `Bearer ${sessionStorage.JWT}`,
-        },
-    })
-        .then((response) => {
-            console.log(response)
-        }).catch((erro) => {
-            console.log(erro)
-        })
-}
-
-function downloadCsv() {
-    api.get(`/petshops/download/csv/${sessionStorage.ID_PETSHOP}`, {
-        headers: {
-            Authorization: `Bearer ${sessionStorage.JWT}`,
-        },
-    })
-        .then((response) => {
-            console.log(response)
-        }).catch((erro) => {
-            console.log(erro)
-        })
-}
+import { ToastComponent } from "../Toast/Toast";
+import LinhaTabela from "./components/LinhaTabela";
 
 function Calendario() {
     const [hour, setHour] = useState();
     const [date, setDate] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(null);
     const [showModal, setShowModal] = useState(false);
-    const [pedido, setPedido] = useState({});
+    const [listaPedido, setListaPedido] = useState([]);
+    const [formatedDate, setFormatedDate] = useState();
 
     const onChange = (newDate) => {
         setDate(newDate);
@@ -52,36 +20,92 @@ function Calendario() {
 
     const handleDayClick = (value) => {
         setSelectedDate(value);
-        console.log(value.toISOString());
-        getInformacoesPedido(selectedDate);
-        setShowModal(true);
+        setFormatedDate(value.toISOString().substring(0, 19))
     };
 
     const closeModal = () => {
+        setPedido({});
         setShowModal(false);
     };
 
     const acceptSchedule = () => {
         date.setHours(13, 10)
-        console.log(date.toISOString())
+    }
+
+    function formatDate(date) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+
+        return `${day < 10 ? "0" + day : day}/${month < 10 ? "0" + month : month}/${year}`;
+    }
+
+    function downloadTxt() {
+        fetch(`http://localhost:8080/petshops/download/txt/${sessionStorage.ID_PETSHOP}`, {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.JWT}`
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                }
+                throw new Error('Erro ao realizar o download');
+            })
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'arquivo.txt';
+                link.click();
+                URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    function downloadCsv() {
+        fetch(`http://localhost:8080/petshops/download/csv/${sessionStorage.ID_PETSHOP}`, {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.JWT}`
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                }
+                throw new Error('Erro ao realizar o download');
+            })
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'arquivo.txt';
+                link.click();
+                URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 
     function getInformacoesPedido(date) {
         api
-            .get(`/report/agendamento/${sessionStorage.ID_PETSHOP}`, {
+            .get(`/agendamentos/report/agendamento/${sessionStorage.ID_PETSHOP}`, {
                 params: {
-                    dataHora: date
+                    dataHora: formatedDate
                 },
                 headers: {
                     Authorization: `Bearer ${sessionStorage.JWT}`
                 }
             })
             .then(({ data }) => {
-                console.log(data);
-                setPedido(data);
+                setListaPedido(data);
+                setShowModal(true);
             })
             .catch((error) => {
-                console.log(error);
+                ToastComponent("Nenhum pedido encontrado na data selecionada", "", true, false)
             });
     }
 
@@ -96,8 +120,9 @@ function Calendario() {
             </div>
 
             <div className="btn-importacao-meus-agendamentos">
-                <button onClick={() => downloadCsv()} id='btn-csv'>Baixar CSV</button>
-                <button onClick={() => downloadTxt()} id='btn-txt'>Baixar TXT</button>
+                <button onClick={() => downloadCsv()} className='btn-laranja'>Baixar CSV</button>
+                <button onClick={() => getInformacoesPedido(selectedDate)} className="btn-roxo">Exibir agendamentos</button>
+                <button onClick={() => downloadTxt()} className='btn-laranja'>Baixar TXT</button>
             </div>
 
             {showModal && (
@@ -115,39 +140,26 @@ function Calendario() {
                         {
                             <div className="content-modal-calendario">
 
-                                <div className="container-modal-calendario">
-
-                                    <div className="retangulo-calendario">
-
-                                        <div className="ajuste-espacamento-calendario">
-                                            <label htmlFor="">Cliente</label>
-                                            <input type="text" value={pedido.nomeCliente} disabled />
-                                        </div>
-
-                                        <div className="ajuste-espacamento-calendario">
-                                            <label htmlFor="">Horário</label>
-                                            <input type="time" value={pedido.dataHora} onChange={(e) => setHour(e.target.value)} />
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                <div className="container-modal-calendario">
-
-                                    <div className="retangulo-calendario">
-
-                                        <div className="ajuste-espacamento-calendario">
-                                            <label htmlFor="">Serviço</label>
-                                            <input type="text" value={pedido.servico} disabled />
-                                        </div>
-
-                                        <div className="ajuste-espacamento-calendario">
-                                            <label htmlFor="">Data</label>
-                                            <input type="text" value={() => formatDate(pedido.dataHora)} disabled />
-                                        </div>
-
-                                    </div>
-
+                                <div className="tabela-meus-pets">
+                                    <table className="table-container">
+                                        <thead>
+                                            <tr>
+                                                <th>Cliente</th>
+                                                <th>Espécie</th>
+                                                <th>Sexo</th>
+                                                <th>Excluir</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {listaPedido.map((pedido) => (
+                                                <LinhaTabela
+                                                    nomeCliente={pedido.nomeCliente}
+                                                    hora={pedido.DataHora.substring(11, 19)}
+                                                    servico={pedido.servico}
+                                                />
+                                            ))}
+                                        </tbody>
+                                    </table>
                                 </div>
 
                             </div>
