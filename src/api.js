be4cb45fbaf2129f17dbb13ab33e59d74ce31509
MauricioMSoares <mauricio.soares@sptech.school>
2023-06-19 22:31:17 -0300

    axios apontando para o localhost

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://petsup-aplication.azurewebsites.net"
+    //baseURL: "https://petsup-aplication.azurewebsites.net"
+    baseURL: "http://localhost:8080"
 })
 
 export default api;
